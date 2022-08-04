630b8b12a7c344cf7f11a3972bcde6b26cc38020
Thiago Gonçalves Coura <thiagocoura@gmail.com>
2022-08-04 11:44:40 -0300

    Update Principais Comandos Linguagem R.R

--- Principais Comandos Linguagem R.R
+++ Principais Comandos Linguagem R.R
@@ -24,10 +24,42 @@ sprintf("valores = %s, %s, %s, %s", num, text, caracter, logical)
 sprintf("valores = %s, %s, %s, %s", class(num), class(text), class(caracter), class(logical))
 
 # vetores:
-
 vetor_num = c(1, 2, 3)
 vetor_text = c("Thiago", "Gonçalves", "Coura")
 
 # cat() concatenate and print  
 cat("Tipo = ", class(vetor_num), class(vetor_text))
+
+cat("Antes: ", vetor_num, \n)
+
+# mundando valores no vetor (indices no vetor começam em 1):
+vetor_num[2] = 27
+
+cat("Mudança: ", vetor_num)
+
+# funções com vetor: 
+x = c(1, -2, 3)
+x = x + 1
+y = seq(1, 10, 0.5)
+z = c(1:10)
+t = rep(c(1,2), each=7)
+
+cat(x, '\n', y, '\n', z, '\n', t)
+print(x+1)
+print(x*3)
+print(sum(y))
+print(max(z), min(z))
+print(sqrt(y))
+print(z > 5)
+l = z > 7
+print(x[l])
+
+
+
+
+
+
+
+
+
 
