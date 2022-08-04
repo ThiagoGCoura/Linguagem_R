0d7b7ae9b880d0af87857c7480688fb808c22a25
Thiago Gonçalves Coura <thiagocoura@gmail.com>
2022-08-04 11:22:17 -0300

    Create Principais Comandos Linguagem R.R

---
+++ Principais Comandos Linguagem R.R
@@ -0,0 +1,33 @@
+# adição:
+5 + 3 
+# subtração:
+5 - 3
+# multiplicação:
+3 * 5
+# divisao:
+(5 + 5) / 2
+
+# equivalente = <- e sensivel maisc e minsc:
+var1 = 1
+Var1 <- 1
+print(var1)
+print(Var1)
+
+#tipos de dados:
+num = 45.55
+text = "texto"
+caracter = 'x'
+logical = TRUE
+sprintf("valores = %s, %s, %s, %s", num, text, caracter, logical)
+
+# imprimir tipo de dado:
+sprintf("valores = %s, %s, %s, %s", class(num), class(text), class(caracter), class(logical))
+
+# vetores:
+
+vetor_num = c(1, 2, 3)
+vetor_text = c("Thiago", "Gonçalves", "Coura")
+
+# cat() concatenate and print  
+cat("Tipo = ", class(vetor_num), class(vetor_text))
+
